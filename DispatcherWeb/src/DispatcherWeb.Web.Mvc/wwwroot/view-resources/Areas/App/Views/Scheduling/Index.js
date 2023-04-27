ce9b7331d0c6f7c01bee022e49db678ae056c800
Eph <ephraim@dumptruckdispatcher.com>
2023-04-27 09:46:56 +0800

    #12616  - refactored to use isPastDate instead

--- DispatcherWeb/src/DispatcherWeb.Web.Mvc/wwwroot/view-resources/Areas/App/Views/Scheduling/Index.js
+++ DispatcherWeb/src/DispatcherWeb.Web.Mvc/wwwroot/view-resources/Areas/App/Views/Scheduling/Index.js
@@ -1813,11 +1813,8 @@
                 }
             },
             preDrawCallback: function (settings) {
-                var currentDate = new Date();
-                var filterDate = new Date(moment($("#DateFilter").val(), 'MM/DD/YYYY'));
-
                 // check if filter includes current day or futures dates
-                if (filterDate >= currentDate) {
+                if (!isPastDate()) {
                     scheduleGrid.settings().context[0].oLanguage.sEmptyTable = "There are no jobs for this date. <button id='#howToAddaJob'>Click here to see how to add a job</button>";
                 } else {
                     scheduleGrid.settings().context[0].oLanguage.sEmptyTable = "No data available in table";
