aab534232b4795887e4e95b67cbdcb272990a535
Aleksey Glukharev <neogarrigus@gmail.com>
2023-05-18 18:55:18 +0200

    #12756 Don't add a second page to printed quote if there's no data to display

--- DispatcherWeb/src/DispatcherWeb.Application/Quotes/QuoteReportGenerator.cs
+++ DispatcherWeb/src/DispatcherWeb.Application/Quotes/QuoteReportGenerator.cs
@@ -286,16 +286,19 @@ namespace DispatcherWeb.Quotes
 
             // Second Page
 
-            section.AddPageBreak();
-            paragraph = document.LastSection.AddParagraph();
-            paragraph.Format.Font.Size = Unit.FromPoint(7.5);
+            if (!string.IsNullOrEmpty(model.QuoteGeneralTermsAndConditions))
+            {
+                section.AddPageBreak();
+                paragraph = document.LastSection.AddParagraph();
+                paragraph.Format.Font.Size = Unit.FromPoint(7.5);
 
-            paragraph.AddLineBreak();
-            paragraph.AddLineBreak();
-            paragraph.AddLineBreak();
-            paragraph.AddText(model.QuoteGeneralTermsAndConditions);
-            paragraph.AddLineBreak();
-            paragraph.AddLineBreak();
+                paragraph.AddLineBreak();
+                paragraph.AddLineBreak();
+                paragraph.AddLineBreak();
+                paragraph.AddText(model.QuoteGeneralTermsAndConditions);
+                paragraph.AddLineBreak();
+                paragraph.AddLineBreak();
+            }
 
             return document.SaveToBytesArray();
         }
