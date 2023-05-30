a8215f9c714f9d7e5e94b4a4cd1d93e3d15f729e
Eph <koyaip.kuro@gmail.com>
2023-05-30 14:02:54 +0800

    clean up

--- DispatcherWeb/src/DispatcherWeb.Web.Mvc/ClientApp/src/navigation/RouterConfig.js
+++ DispatcherWeb/src/DispatcherWeb.Web.Mvc/ClientApp/src/navigation/RouterConfig.js
@@ -2,13 +2,6 @@ import React, { useEffect } from 'react';
 import { Routes, Route } from 'react-router';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { authProtectedRoutes } from './index';
-import Dashboard from '../pages/Dashboard';
-import Customers from '../pages/Customers';
-import ProductsOrServices from '../pages/ProductsOrServices';
-import Drivers from '../pages/Drivers';
-import Locations from '../pages/Locations';
-import Schedule from '../pages/Schedule';
-import TruckDispatchList from '../pages/TruckDispatchList';
   
 export const RouterConfig = ({
     isAuthenticated,
@@ -18,16 +11,6 @@ export const RouterConfig = ({
     const queryParams = new URLSearchParams(location.search);
     const targetRoute = queryParams.get('route');
     const navigate = useNavigate();
-
-    const routes = [
-        { path: '/dashboard', component: Dashboard },
-        { path: '/customers', component: Customers },
-        { path: '/products-services', component: ProductsOrServices },
-        { path: '/drivers', component: Drivers },
-        { path: '/locations', component: Locations },
-        { path: '/dispatching/schedule', component: Schedule },
-        { path: '/dispatching/dispatches/truck-list', component: TruckDispatchList },
-    ];
 
     useEffect(() => {
         if (targetRoute) {
@@ -40,14 +23,12 @@ export const RouterConfig = ({
             { isAuthenticated && 
                 <Routes sx={{ height: '100%', overflow: 'auto' }}>
                     { authProtectedRoutes.map((route, index) => {
-                        const Component = route.component;
-        
                         return (
                             <Route
                                 key={index}
                                 path={route.path}
                                 element={
-                                    <Component handleCurrentPageName={handleCurrentPageName} />
+                                    <route.component handleCurrentPageName={handleCurrentPageName} />
                                 }
                             />
                         );
