1cf16db96acc7ea9645f90ebdec914547d257b65
Aleksey Glukharev <neogarrigus@gmail.com>
2023-06-14 19:59:22 +0200

    #12789 Prevent duplicate driver assignments, remove null driver assignments if other driver assignments exist

---
+++ DispatcherWeb/src/DispatcherWeb.Application.Shared/DriverAssignments/Dto/RemoveDuplicateDriverAssignmentsInput.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DispatcherWeb.DriverAssignments.Dto
+{
+    public class RemoveDuplicateDriverAssignmentsInput
+    {
+        public DateTime Date { get; set; }
+        public Shift? Shift { get; set; }
+        public int OfficeId { get; set; }
+        public int TruckId { get; set; }
+    }
+}
