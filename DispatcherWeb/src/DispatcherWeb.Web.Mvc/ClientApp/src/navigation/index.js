a4fc4113d53a905b85c0f2ccdebef5acd3418753
Kevin Aton <kevinaton@Kevins-MacBook-Pro.local>
2023-06-26 22:47:59 +0800

    #12768 added modal for add and edit quote

--- DispatcherWeb/src/DispatcherWeb.Web.Mvc/ClientApp/src/navigation/index.js
+++ DispatcherWeb/src/DispatcherWeb.Web.Mvc/ClientApp/src/navigation/index.js
@@ -5,6 +5,7 @@ import Drivers from '../pages/Drivers';
 import Locations from '../pages/Locations';
 import Schedule from '../pages/Schedule';
 import TruckDispatchList from '../pages/TruckDispatchList';
+import Quotes from '../pages/Quotes';
 
 const authProtectedRoutes = [
     { path: '/dashboard', component: Dashboard },
@@ -13,7 +14,8 @@ const authProtectedRoutes = [
     { path: '/drivers', component: Drivers },
     { path: '/locations', component: Locations },
     { path: '/dispatching/schedule', component: Schedule },
-    { path: '/dispatching/dispatches/truck-list', component: TruckDispatchList }
-]
+    { path: '/dispatching/dispatches/truck-list', component: TruckDispatchList },
+    { path: '/quote', component: Quotes },
+];
 
-export { authProtectedRoutes }
+export { authProtectedRoutes };
